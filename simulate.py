fc4ad7a90396c1bcc83ffe15671e5d9a282d3e5d
csvelayutham <cs_velayutham@cb.amrita.edu>
2023-10-11 17:49:44 +0530

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(0.01)
+    print(_)
 
 p.disconnect()
